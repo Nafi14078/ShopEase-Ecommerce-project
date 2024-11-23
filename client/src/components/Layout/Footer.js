683a7aee2b133341b1f12d923d319ef59c4684dc
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-11-23 17:03:47 +0600

    fifth commit

--- client/src/components/Layout/Footer.js
+++ client/src/components/Layout/Footer.js
@@ -1,10 +1,16 @@
 import React from 'react'
+import { Link } from 'react-router-dom';
 
 const Footer = () => {
   return (
-    <div className='bg-dark text-light p-3'>
+    <div className='footer'>
 
-        <h4 className="text-center">All Right Reserved &copy; ShopEase</h4>
+        <h5 className="text-center">All Right Reserved &copy; ShopEase</h5>
+        <p className='text-center mt-3'>
+          <Link to="/about">About</Link>
+          <Link to="/contact">Contact</Link>
+          <Link to="/policy">Privacy Policy</Link>
+        </p>
     </div>
   );
 };
