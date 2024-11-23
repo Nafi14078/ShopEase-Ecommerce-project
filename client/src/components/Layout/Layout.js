74f586c045045dfa0f47a2dd05897942cb8fb980
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-11-23 17:03:47 +0600

    fifth commit

--- client/src/components/Layout/Layout.js
+++ client/src/components/Layout/Layout.js
@@ -1,20 +1,32 @@
 import React from "react";
 import Header from "./Header";
 import Footer from "./Footer";
+import { Helmet } from "react-helmet";
 
-const Layout=({children})=>{
-    return (
-        <div>
-            <Header/>
-            <main style={{minHeight:'80vh'}}>
-            {children}
+const Layout = ({ children, title, description, keywords, author }) => {
+  return (
+    <div>
+      <Helmet>
+        <meta charSet="utf-8" />
+        
+          <meta name="description" content={description} />
+          <meta name="keywords" content={keywords} />
+          <meta name="author" content={author} />
+        
 
-            </main>
-            <Footer/>
+        <title>{title}</title>
+      </Helmet>
+      <Header />
+      <main style={{ minHeight: "75vh" }}>{children}</main>
+      <Footer />
+    </div>
+  );
+};
 
-            
-            
-        </div>
-    );
+Layout.defaultProps={
+    title:"ShopEase-shope now",
+    description:"mern stact project",
+    keywords:"mern,react,node,mongoDB",
+    author:"ShopEase",
 };
 export default Layout;
