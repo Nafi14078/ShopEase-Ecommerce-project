846eeacf36c4f7818eb4bf010849071cf469cb1f
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-11-24 10:55:51 +0600

    sixth commit

--- controllers/authController.js
+++ controllers/authController.js
@@ -8,19 +8,19 @@ export const registerController=async(req,res)=>{
 
         //validation
         if(!name){
-            return res.send({error:'Name is Required'})
+            return res.send({message:'Name is Required'})
         }
         if(!email){
-            return res.send({error:'Email is Required'})
+            return res.send({message:'Email is Required'})
         }
         if(!password){
-            return res.send({error:'Password is Required'})
+            return res.send({message:'Password is Required'})
         }
         if(!phone){
-            return res.send({error:'Phone no is Required'})
+            return res.send({message:'Phone no is Required'})
         }
         if(!address){
-            return res.send({error:'Address is Required'})
+            return res.send({message:'Address is Required'})
         }
 
         //check user
@@ -28,7 +28,7 @@ export const registerController=async(req,res)=>{
         //existing user
         if(existingUser){
             return res.status(200).send({
-                success:true,
+                success:false,
                 message:'Already Register please login',
             })
         }
@@ -106,5 +106,10 @@ export const loginController=async(req,res)=>{
             error
         })
     }
+};
+
+//test controller
+export const testController=(req,res)=>{
+   res.send('protected Routes');
 };
 
