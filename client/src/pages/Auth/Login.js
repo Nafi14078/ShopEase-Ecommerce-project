d8eb724088f098c4a4025e44a1cadfd8eba81e2e
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-11-25 23:51:32 +0600

    eight

--- client/src/pages/Auth/Login.js
+++ client/src/pages/Auth/Login.js
@@ -71,9 +71,19 @@ const Login = () => {
             />
           </div>
     
+          <div className="mb-3">
           <button type="submit" className="btn btn-primary">
             LOGIN
           </button>
+
+          </div>
+          
+          <div className="mb-3">
+          <button type="button" className="btn btn-primary" onClick={()=>{navigate('/forgot-password')}}>
+            Forgot Password
+          </button>
+
+          </div>
         </form>
       </div>
     </Layout>
