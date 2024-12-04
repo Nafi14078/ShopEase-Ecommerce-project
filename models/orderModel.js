1666a0258a2efe1498be0e6a4f0f0b4674baf424
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-12-04 22:42:54 +0600

    16th

--- models/orderModel.js
+++ models/orderModel.js
@@ -1,25 +1,25 @@
 import mongoose from "mongoose";
 
-const orderSchema=new mongoose.Schema({
-    products:[{
-        type:mongoose.ObjectId,
-        ref:"Product",
-
+const orderSchema = new mongoose.Schema(
+  {
+    products: [
+      {
+        type: mongoose.ObjectId,
+        ref: "Products",
+      },
+    ],
+    payment: {},
+    buyer: {
+      type: mongoose.ObjectId,
+      ref: "users",
     },
-],
-payment:{},
-buyer:{
-    type:mongoose.ObjectId,
-    ref:'users',
-},
-status: {
-    type: String,
-    default: "Not Process",
-    enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+    status: {
+      type: String,
+      default: "Not Process",
+      enum: ["Not Process", "Processing", "Shipped", "deliverd", "cancel"],
+    },
   },
-},
-{ timestamps: true }
-
+  { timestamps: true }
 );
 
-export default mongoose.model("Order",orderSchema);
+export default mongoose.model("Order", orderSchema);
