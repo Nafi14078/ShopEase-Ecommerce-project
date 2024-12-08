8760140b6683dc0b8a6fcd82aa4f9837ccd204cd
Ashfak Azad Nafi <ashfakazadnafi@gmail.com>
2024-12-08 16:37:55 +0600

    19th

--- client/src/pages/ProductDetails.js
+++ client/src/pages/ProductDetails.js
@@ -84,7 +84,7 @@ const ProductDetails = () => {
               />
               <div className="card-body">
                 <h5 className="card-title">{p.name}</h5>
-                <p className="card-text">{p.description.substring(0, 30)}...</p>
+                <p className="card-text">{p.description}</p>
                 <p className="card-text"> $ {p.price}</p>
                 <div>
                   <button
